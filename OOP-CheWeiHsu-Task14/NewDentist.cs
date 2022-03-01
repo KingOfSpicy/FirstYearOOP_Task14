541324fb6aefdff6a267bf354ac651c8634b2c36
KingOfSpicy <sangowosan@gmail.com>
2022-03-01 11:56:46 +0200

    add dentists class.

---
+++ OOP-CheWeiHsu-Task14/NewDentist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace OOP_CheWeiHsu_Task14
+{
+    class NewDentist
+    {
+        public NewDentist(int id, string name, string telNum)
+        {
+            Id = id;
+            Name = name;
+            TelNum = telNum;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TelNum { get; set; }
+
+        public GetAllDentists()
+        {
+
+        }
+    }
+}
